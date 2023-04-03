8971150cdc33ce9a8147e79f60d5164f27c32010
Val2403 <121095277+Val2403@users.noreply.github.com>
2023-04-03 09:40:40 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 user_token = ''# токен пользователя
 comm_token = ''# токен сообщества
 
-host = '127.0.0.1' 
-user = 'postgres'
-password = '0000'
-db_name = 'bot_users'
+host = '...' 
+user = '...'
+password = '...'
+db_name = '...'
