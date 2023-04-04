9dc6f10d75afcaa085e90bc54dfd2548ade236b5
Val2403 <121095277+Val2403@users.noreply.github.com>
2023-04-04 12:38:14 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,10 +3,11 @@ from main import bot
 from database import creating_database
 from vk_api.longpoll import VkEventType
 
+
 class VKBot:
-    offset = 0 #сдвиг
-    line = range(0, 1000) # последовательность для перебора найденных пользователей
-    
+    offset = 0  # сдвиг
+    line = range(0, 1000)  # последовательность для перебора найденных пользователей
+
     while True:
         for event in bot.longpoll.listen():
             if event.type == VkEventType.MESSAGE_NEW and event.to_me:
@@ -14,18 +15,21 @@ class VKBot:
                 user_id = str(event.user_id)
                 msg = event.text.lower()
                 sender(user_id, msg.lower())
-                if request == 'начать поиск':
+                if request == "начать поиск":
                     creating_database()
-                    bot.write_msg(user_id, f'Привет, {bot.get_user_name(user_id)}')
+                    bot.write_msg(user_id, f"Привет, {bot.get_user_name(user_id)}")
                     bot.find_user(user_id)
-                    bot.write_msg(event.user_id, f'Я нашёл подходящую для тебя пару, жми на кнопку "Вперёд"')
+                    bot.write_msg(
+                        event.user_id,
+                        f'Я нашёл подходящую для тебя пару, жми на кнопку "Вперёд"',
+                    )
                     bot.find_persons(user_id, offset)
 
-                elif request == 'вперёд':
+                elif request == "вперёд":
                     for i in line:
                         offset += 1
                         bot.find_persons(user_id, offset)
                         break
 
                 else:
-                    bot.write_msg(event.user_id, 'Твоё сообщение не совсем понятно')
+                    bot.write_msg(event.user_id, "Твоё сообщение не совсем понятно")
